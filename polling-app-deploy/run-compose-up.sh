d4949bae5df39344ab1dd0c04debd260a1224261
Henrique Tornelli Duarte <henrique.tornelli@ilinksolutions.com.br>
2020-12-11 21:04:30 -0300

    dockerizing project

---
+++ polling-app-deploy/run-compose-up.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Copying backend and frontend builds to artifacts folders
+cp ../polling-app-server/target/polls-*.jar ./backend/artifacts/polls.jar
+cp -r ../polling-app-client/build/* ./frontend/artifacts/
+
+docker pull openjdk:8-jdk-alpine
+docker-compose build
+docker-compose down
+docker-compose up -d
