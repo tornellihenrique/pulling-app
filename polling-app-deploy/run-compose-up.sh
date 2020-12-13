cb9a6900ae16a05e3f99c37aa293adbdead3445f
Henrique Tornelli Duarte <henrique.tornelli@ilinksolutions.com.br>
2020-12-13 12:00:17 -0300

    configuring mysql volume

--- polling-app-deploy/run-compose-up.sh
+++ polling-app-deploy/run-compose-up.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Creating local database volume folder
+mkdir -p /tmp/datadir
+
 # Copying backend and frontend builds to artifacts folders
 cp ../polling-app-server/target/polls-*.jar ./backend/artifacts/polls.jar
 cp -r ../polling-app-client/build/* ./frontend/artifacts/
